af615eb015117d53f7fe38445f65daa679e5fcae
Adrian Utomo <adrianhakimutomo@gmail.com>
2025-05-18 15:14:39 +0700

    chore: change widndow to fixed

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,9 @@ if (require('electron-squirrel-startup')) {
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
+    height: 500,
+    width: 500,
+    resizable: false,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
