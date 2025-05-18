d9f0220f53feb49d2d35516aeb1ad87e49ed8697
Adrian Utomo <adrianhakimutomo@gmail.com>
2025-05-18 15:15:13 +0700

    chore: just playing around

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 export default function App() {
+  const [breakfast, setBreakfast] = useState("adrian");
   return (
-    <div className="text-green-700">
-      hello world this is from reactsdfsd
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="text-green-700">{breakfast}</div>
+      <button
+        onClick={() =>
+          setBreakfast((prev) => (prev === "adrian" ? "butskow" : "adrian"))
+        }
+        className="bg-blue-400 rounded-md p-2"
+      >
+        click to change state
+      </button>
     </div>
-  )
+  );
 }
