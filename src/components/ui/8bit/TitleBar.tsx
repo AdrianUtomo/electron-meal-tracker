c25948c9bf195b56cdd62e93501859311eaf16df
Adrian Utomo <adrianhakimutomo@gmail.com>
2025-06-23 20:29:40 +0700

    feat: title bar buttons

--- src/components/ui/8bit/TitleBar.tsx
+++ src/components/ui/8bit/TitleBar.tsx
@@ -2,40 +2,35 @@ import { Icon } from "@iconify/react";
 import React from "react";
 
 interface TitleBarProps {
-    title: string;
+  title: string;
 }
 
 export function TitleBar({ title }: TitleBarProps) {
-    const handleMinimize = () => {
-        window.electron.ipcRenderer.send("window-minimize");
-    };
+  const handleMinimize = () => {
+    window.electron.ipcRenderer.send("window-minimize");
+  };
 
-    const handleClose = () => {
-        window.electron.ipcRenderer.send("window-close");
-    };
+  const handleClose = () => {
+    window.electron.ipcRenderer.send("window-close");
+  };
 
-    return (
-        <div
-            onClick={() => console.log("you pressed the title bar")}
-            className="flex items-center justify-end w-full h-12 bg-[#fec7cd] border-b-4 border-[#c45363] px-2"
-            style={{ WebkitAppRegion: "drag" } as React.CSSProperties}
-        >
-            <div className="flex gap-2" style={{ WebkitAppRegion: "no-drag" } as React.CSSProperties}>
-                <button onClick={handleMinimize}>
-                    <Icon
-                        icon="pixelarticons:minus"
-                        width="24"
-                        height="24"
-                    />
-                </button>
-                <button onClick={handleClose}>
-                    <Icon
-                        icon="pixelarticons:close-box"
-                        width="24"
-                        height="24"
-                    />
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      onClick={() => console.log("you pressed the title bar")}
+      className="flex items-center justify-end w-full h-12 bg-[#fec7cd] border-b-4 border-[#c45363] px-2"
+      style={{ WebkitAppRegion: "drag" } as React.CSSProperties}
+    >
+      <div
+        className="flex items-center gap-2"
+        style={{ WebkitAppRegion: "no-drag" } as React.CSSProperties}
+      >
+        <button onClick={handleMinimize} className="border-3 border-[#d6697b] p-[2px] cursor-pointer">
+          <Icon icon="pixelarticons:minus" width="20" height="20" />
+        </button>
+				<button onClick={handleClose} className="cursor-pointer">
+          <Icon icon="pixelarticons:close-box" width="40" height="40" />
+        </button>
+      </div>
+    </div>
+  );
 }
